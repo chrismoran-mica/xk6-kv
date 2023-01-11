157c0f627dc5f6d1ead01f856b3fb6a657dd2526
Chris Moran <chris.moran@mica.io>
2023-01-11 07:12:59 -0500

    merged github.com/j-etienne/xk6-kv for better modules support for xk6-kv

--- kv.go
+++ kv.go
@@ -2,188 +2,162 @@ package kv
 
 import (
 	"fmt"
+	"time"
+
+	badger "github.com/dgraph-io/badger/v3"
 	"github.com/dop251/goja"
 	"go.k6.io/k6/js/modules"
-	"os"
-	"strings"
-	"sync"
-	"time"
 )
+
+type (
+	// KV is the global module instance that will create Client
+	// instances for each VU.
+	KV struct{}
+
+	// ModuleInstance represents an instance of the JS module.
+	ModuleInstance struct {
+		vu modules.VU
+		*Client
+	}
+)
+
+// Ensure the interfaces are implemented correctly
+var (
+	_ modules.Instance = &ModuleInstance{}
+	_ modules.Module   = &KV{}
+)
+
+type Client struct {
+	vu modules.VU
+	db *badger.DB
+}
+
+var check = false
+var client *Client
 
 func init() {
 	modules.Register("k6/x/kv", New())
 }
 
-type (
-	// RootModule is the global module instance that will create module
-	// instances for each VU.
-	RootModule struct{}
-
-	// ModuleInstance represents an instance of the JS module.
-	ModuleInstance struct {
-		// vu provides methods for accessing internal k6 objects for a VU
-		vu modules.VU
-		// kv is the exported type
-		exports map[string]interface{}
-	}
-)
-
-// Ensure the interfaces are implemented correctly.
-var (
-	_      modules.Instance = &ModuleInstance{}
-	_      modules.Module   = &RootModule{}
-	once   sync.Once
-	client *Client
-)
-
-// New returns a pointer to a new RootModule instance.
-func New() *RootModule {
-	return &RootModule{}
+// New returns a pointer to a new KV instance
+func New() *KV {
+	return &KV{}
 }
 
-// KV is the k6 key-value extension.
-type KV struct {
-	vu      modules.VU
-	exports map[string]interface{}
+// NewModuleInstance implements the modules.Module interface and returns
+// a new instance for each VU.
+func (*KV) NewModuleInstance(vu modules.VU) modules.Instance {
+	return &ModuleInstance{vu: vu, Client: &Client{vu: vu}}
 }
 
-type ValueTTL struct {
-	value interface{}
-	tm    *time.Timer
-}
-
-type Client struct {
-	db map[string]*ValueTTL
-	mu sync.Mutex
-}
-
-// NewModuleInstance implements the modules.Module interface returning a new instance for each VU.
-func (r *RootModule) NewModuleInstance(vu modules.VU) modules.Instance {
-	mi := &ModuleInstance{
-		vu:      vu,
-		exports: make(map[string]interface{}),
-	}
-
-	mi.exports["Client"] = mi.newClient
-
-	return mi
-}
-
-// Exports implements the modules.Instance interface and returns the exported types for the JS module.
+// Exports implements the modules.Instance interface and returns
+// the exports of the JS module.
 func (mi *ModuleInstance) Exports() modules.Exports {
 	return modules.Exports{
-		Named:   mi.exports,
-		Default: newClient(),
+		Named: map[string]interface{}{
+			"Client": mi.NewClient,
+		}}
+}
+
+// NewClient is the JS constructor for the Client
+func (mi *ModuleInstance) NewClient(call goja.ConstructorCall) *goja.Object {
+	rt := mi.vu.Runtime()
+
+	var name = ""
+	var memory = false
+	if len(call.Arguments) == 1 {
+		name = call.Arguments[0].String()
 	}
-}
 
-func (mi *ModuleInstance) newClient(_ goja.ConstructorCall, rt *goja.Runtime) *goja.Object {
-	return rt.ToValue(newClient()).ToObject(rt)
-}
+	if len(call.Arguments) == 2 {
+		name = call.Arguments[0].String()
+		memory = call.Arguments[1].ToBoolean()
+	}
 
-func newClient() *Client {
-	once.Do(func() {
-		db := make(map[string]*ValueTTL, 500)
-		client = &Client{db: db}
-	})
-	return client
+	if check != true {
+		if name == "" {
+			name = "/tmp/badger"
+		}
+		var db *badger.DB
+		if memory {
+			db, _ = badger.Open(badger.DefaultOptions("").WithLoggingLevel(badger.ERROR).WithInMemory(true))
+		} else {
+			db, _ = badger.Open(badger.DefaultOptions(name).WithLoggingLevel(badger.ERROR))
+		}
+		client = &Client{vu: mi.vu, db: db}
+		check = true
+	}
+
+	return rt.ToValue(client).ToObject(rt)
 }
 
 // Set the given key with the given value.
-func (c *Client) Set(key string, value interface{}) error {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	var cv *ValueTTL
-	var ok bool
-	if cv, ok = c.db[key]; ok {
-		if cv.tm != nil {
-			stopped := cv.tm.Stop()
-			if !stopped {
-				_, _ = fmt.Fprintf(os.Stderr, "set: wtf how the...?\n")
-				<-cv.tm.C
-			}
-		}
-		cv.value = value
-		cv.tm = nil
-	} else {
-		cv = &ValueTTL{
-			value: value,
-			tm:    nil,
-		}
-	}
-	c.db[key] = cv
-	return nil
+func (c *Client) Set(key string, value string) error {
+	err := c.db.Update(func(txn *badger.Txn) error {
+		err := txn.Set([]byte(key), []byte(value))
+		return err
+	})
+	return err
 }
 
-// SetWithTTLInSecond the given key with the given value with TTL in second
-func (c *Client) SetWithTTLInSecond(key string, value interface{}, ttl int64) error {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	var cv *ValueTTL
-	var ok bool
-	if cv, ok = c.db[key]; ok {
-		if cv.tm != nil {
-			stopped := cv.tm.Stop()
-			if !stopped {
-				_, _ = fmt.Fprintf(os.Stderr, "setWithTTLInSecond: wtf how the...?\n")
-				<-cv.tm.C
-			}
-		}
-		cv.value = value
-	} else {
-		cv = &ValueTTL{
-			value: value,
-		}
-	}
-	cv.tm = time.NewTimer(time.Duration(ttl) * time.Second)
-	c.db[key] = cv
-	go func() {
-		<-cv.tm.C
-		c.mu.Lock()
-		delete(c.db, key)
-		c.mu.Unlock()
-	}()
-	return nil
+// SetWithTTLInSecond Sets the given key with the given value with TTL in second
+func (c *Client) SetWithTTLInSecond(key string, value string, ttl int) error {
+	err := c.db.Update(func(txn *badger.Txn) error {
+		e := badger.NewEntry([]byte(key), []byte(value)).WithTTL((time.Duration(ttl) * time.Second))
+		err := txn.SetEntry(e)
+		return err
+	})
+	return err
 }
 
 // Get returns the value for the given key.
-func (c *Client) Get(key string) (interface{}, error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	if cv, ok := c.db[key]; ok {
-		return cv.value, nil
+func (c *Client) Get(key string) (string, error) {
+	var valCopy []byte
+	_ = c.db.View(func(txn *badger.Txn) error {
+		item, _ := txn.Get([]byte(key))
+		if item != nil {
+			valCopy, _ = item.ValueCopy(nil)
+		}
+		return nil
+	})
+	if len(valCopy) > 0 {
+		return string(valCopy), nil
 	}
 	return "", fmt.Errorf("error in get value with key %s", key)
 }
 
 // ViewPrefix return all the key value pairs where the key starts with some prefix.
-func (c *Client) ViewPrefix(prefix string) map[string]interface{} {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	m := make(map[string]interface{})
-	for k, v := range c.db {
-		if strings.HasPrefix(k, prefix) {
-			m[k] = v.value
+func (c *Client) ViewPrefix(prefix string) map[string]string {
+	m := make(map[string]string)
+	_ = c.db.View(func(txn *badger.Txn) error {
+		it := txn.NewIterator(badger.DefaultIteratorOptions)
+		defer it.Close()
+		prefix := []byte(prefix)
+		for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+			item := it.Item()
+			k := item.Key()
+			err := item.Value(func(v []byte) error {
+				m[string(k)] = string(v)
+				return nil
+			})
+			if err != nil {
+				return err
+			}
 		}
-	}
+		return nil
+	})
 	return m
 }
 
 // Delete the given key
 func (c *Client) Delete(key string) error {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	if cv, ok := c.db[key]; ok {
-		if cv.tm != nil {
-			stopped := cv.tm.Stop()
-			if !stopped {
-				_, _ = fmt.Fprintf(os.Stderr, "delete: wtf how the...?\n")
-				<-cv.tm.C
-			}
+	err := c.db.Update(func(txn *badger.Txn) error {
+		item, _ := txn.Get([]byte(key))
+		if item != nil {
+			err := txn.Delete([]byte(key))
+			return err
 		}
-	}
-	delete(c.db, key)
-	return nil
+		return nil
+	})
+	return err
 }
